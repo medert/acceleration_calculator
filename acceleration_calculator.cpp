15896f96610660f46b88e1553fd743464284f724
Meder <m.toktosunov@icloud.com>
2016-02-27 16:00:59 -0500

    changed scanf to scanf_s

--- acceleration_calculator.cpp
+++ acceleration_calculator.cpp
@@ -5,24 +5,24 @@ void instructions(void);
 double calculate_acceleration(double t, double v_i, double v_f);
 double calculate_time_to_stop(double t, double v_i, double v_f);
 
-
 int main(void)
 {
     double t, v_i, v_f, a;
     instructions();
-    
+
     printf("\nPlease, enter the time interval in minutes?\n");
-    scanf("%lf", &t);
-    
+    scanf_s("%lf", &t);
+
     printf("Please, enter the initial velocity in miles per hour?\n");
-    scanf("%lf", &v_i);
-    
+    scanf_s("%lf", &v_i);
+
     printf("Please, enter the final velocity in miles per hour?\n");
-    scanf("%lf", &v_f);
-    
+    scanf_s("%lf", &v_f);
+
     printf("\nThe cyclist decelerates at %.2f miles per hour.\n", calculate_acceleration(t, v_i, v_f));
     printf("To decelerate to a full stop the cyclist will need %.2f minutes.\n", calculate_time_to_stop(t, v_i, v_f));
-    
+
+    getchar();
     getchar();
     return 0;
 }
@@ -37,19 +37,19 @@ void instructions(void)
 double calculate_acceleration(double t, double v_i, double v_f)
 {
     double a;
-    
+
     a = (v_f * 60 - v_i * 60) / t;
 
-    return a;        
+    return a;
 }
 
 double calculate_time_to_stop(double t, double v_i, double v_f)
 {
     double time_to_stop, a;
-    
+
     a = calculate_acceleration(t, v_i, v_f);
-    
+
     time_to_stop = (0 - v_i * 60 ) / a;
-    
+
     return time_to_stop;
 }
